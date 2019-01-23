6ff0d100fcad0f1b36d25b7e3bd218560427b67d
Bill Tomczak <bill@joomlashack.com>
2019-01-22 18:48:49 -0800

    Update sql install/uninstall/update configuration

---
+++ src/admin/sql/install/mysql/uninstall..sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS `#__focalpoint_legends`;
+DROP TABLE IF EXISTS `#__focalpoint_locations`;
+DROP TABLE IF EXISTS `#__focalpoint_locationtypes`;
+DROP TABLE IF EXISTS `#__focalpoint_location_type_xref`;
+DROP TABLE IF EXISTS `#__focalpoint_maps`;
