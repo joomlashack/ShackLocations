9555142abb3c4a450303d104d4a51dc8914c3df5
Bill Tomczak <bill@joomlashack.com>
2019-02-18 11:28:04 -0800

    Remove obsoleted methods

--- src/admin/models/location.php
+++ src/admin/models/location.php
@@ -22,7 +22,9 @@
  * along with ShackLocations.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+use Joomla\CMS\Form\Form;
 use Joomla\CMS\Object\CMSObject;
+use Joomla\CMS\Table\Table;
 
 defined('_JEXEC') or die();
 
@@ -44,7 +46,7 @@ class FocalpointModellocation extends JModelAdmin
      * @param array $data
      * @param bool  $loadData
      *
-     * @return JForm
+     * @return Form
      * @throws Exception
      */
     public function getForm($data = array(), $loadData = true)
@@ -59,210 +61,9 @@ class FocalpointModellocation extends JModelAdmin
     }
 
     /**
-     * @param JForm     $form
-     * @param CMSObject $data
-     * @param string    $group
-     *
-     * @return void
+     * @return array|CMSObject|mixed
      * @throws Exception
      */
-    protected function preprocessForm(\JForm $form, $data, $group = 'content')
-    {
-        parent::preprocessForm($form, $data, $group);
-
-        return;
-        
-        $customFields = $form->getXml()->xpath('//fieldset[@name="customfieldsdata"]');
-        if ($customFields = array_pop($customFields)) {
-            if (empty($data->id) || empty($data->type)) {
-                $customFields['description'] = 'COM_FOCALPOINT_LEGEND_LOCATION_CUSTOMFIELDS_SAVE_FIRST';
-
-            } else {
-                $app = JFactory::getApplication();
-
-                $definedFields = $this->getCustomFields($data->type);
-                if (!$definedFields) {
-                    $customFields['description'] = 'COM_FOCALPOINT_LEGEND_LOCATION_CUSTOMFIELDS_NONE_DEFINED';
-
-                } else {
-                    $group         = $customFields->addChild('fields');
-                    $group['name'] = 'customfields';
-
-                    foreach ($definedFields as $key => $definedField) {
-                        $keyParts = explode('_', $key);
-                        if (count($keyParts) != 2) {
-                            $app->enqueueMessage('Invalid custom field key found - ' . $key, 'error');
-                            continue;
-                        }
-
-                        $keyParts[] = $definedField['name'];
-                        $hashedName = join('_', $keyParts);
-
-                        $fieldAttribs = array(
-                            'name'        => $hashedName,
-                            'label'       => $definedField['label'],
-                            'description' => $definedField['description']
-                        );
-
-                        $dataType = $keyParts[0];
-                        switch ($dataType) {
-                            case 'textbox':
-                                $fieldAttribs['type'] = 'textbox';
-                                $this->addCustomField($group, $fieldAttribs);
-                                break;
-
-                            case 'email':
-                                $emailGroup         = $group->addChild('fields');
-                                $emailGroup['name'] = $hashedName;
-
-                                $spacer = array(
-                                    'type'        => 'spacer',
-                                    'label'       => $fieldAttribs['label'],
-                                    'description' => $fieldAttribs['description']
-                                );
-                                $this->addCustomField($emailGroup, $spacer);
-
-                                $email = array_merge(
-                                    $fieldAttribs,
-                                    array(
-                                        'name'       => 'email',
-                                        'type'       => 'email',
-                                        'label'      => 'Address',
-                                        'descripton' => 'The actual email address. Do not include mailto:. This will be added automatically'
-                                    )
-                                );
-                                $this->addCustomField($emailGroup, $email);
-
-                                $emailText = array_merge(
-                                    $fieldAttribs,
-                                    array(
-                                        'name'        => 'linktext',
-                                        'label'       => 'Text',
-                                        'description' => 'Optional link text. If left blank the URL will be used as link text.'
-                                    )
-                                );
-                                $this->addCustomField($emailGroup, $emailText);
-                                break;
-
-                            case 'textarea':
-                                if ($definedField['loadeditor']) {
-                                    $fieldAttribs['type']   = 'editor';
-                                    $fieldAttribs['filter'] = 'JComponentHelper::filterText';
-                                } else {
-                                    $fieldAttribs['type'] = 'textarea';
-                                }
-                                $this->addCustomField($group, $fieldAttribs);
-                                break;
-
-                            case 'image':
-                                $fieldAttribs['type']      = 'media';
-                                $fieldAttribs['directory'] = $definedField['directory'];
-                                $this->addCustomField($group, $fieldAttribs);
-                                break;
-
-                            case 'link':
-                                $linkGroup         = $group->addChild('fields');
-                                $linkGroup['name'] = $hashedName;
-
-                                $spacer = array(
-                                    'type'        => 'spacer',
-                                    'label'       => $fieldAttribs['label'],
-                                    'description' => $fieldAttribs['description']
-                                );
-                                $this->addCustomField($linkGroup, $spacer);
-
-                                // URL input
-                                $linkUrl = array_merge(
-                                    $fieldAttribs,
-                                    array(
-                                        'name'        => 'url',
-                                        'type'        => 'text',
-                                        'label'       => 'URL',
-                                        'description' => 'The URL to link to. Include http:// at the start for external links.'
-                                    )
-                                );
-                                $this->addCustomField($linkGroup, $linkUrl);
-
-                                // Link text field
-                                $linkText = array_merge(
-                                    $fieldAttribs,
-                                    array(
-                                        'name'        => 'linktext',
-                                        'type'        => 'text',
-                                        'label'       => 'Link text',
-                                        'description' => 'Optional link text. If left blank the URL will be used as link text.'
-                                    )
-                                );
-                                $this->addCustomField($linkGroup, $linkText);
-
-                                // Target field
-                                $linkTarget  = array_merge(
-                                    $fieldAttribs,
-                                    array(
-                                        'name'    => 'target',
-                                        'type'    => 'radio',
-                                        'class'   => 'btn-group btn-group-yesno',
-                                        'label'   => 'Target',
-                                        'default' => 0
-                                    )
-                                );
-                                $targetField = $this->addCustomField($linkGroup, $linkTarget);
-
-                                $yes = $targetField->addChild('option', JText::_('JYES'));
-                                $no  = $targetField->addChild('option', JText::_('JNO'));
-
-                                $yes['value'] = '1';
-                                $no['value']  = '0';
-                                break;
-
-                            case 'selectlist':
-                            case 'multiselect':
-                                $options = preg_split('/\n\r?/', $definedField['options']);
-                                $options = array_map('trim', $options);
-
-                                if ($options) {
-                                    $fieldAttribs['type'] = 'list';
-                                    if ($dataType == 'multiselect') {
-                                        $fieldAttribs['multiple'] = 'true';
-                                    }
-
-                                    $selectField = $this->addCustomField($group, $fieldAttribs);
-                                    foreach ($options as $option) {
-                                        $option          = $selectField->addChild('option', $option);
-                                        $option['value'] = $option;
-                                    }
-                                }
-
-                                break;
-
-                            default:
-                                $fieldAttribs['type'] = $dataType;
-                                $this->addCustomField($group, $fieldAttribs);
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    /**
-     * Add a field to the specified <fields> group using the supplied attributes
-     *
-     * @param SimpleXMLElement $group
-     * @param array            $attributes
-     *
-     * @return SimpleXMLElement
-     */
-    protected function addCustomField(SimpleXMLElement $group, array $attributes)
-    {
-        $field = $group->addChild('field');
-        foreach ($attributes as $attribute => $value) {
-            $field[$attribute] = $value;
-        }
-
-        return $field;
-    }
-
     protected function loadFormData()
     {
         $app  = JFactory::getApplication();
@@ -300,7 +101,7 @@ class FocalpointModellocation extends JModelAdmin
     }
 
     /**
-     * @param JTable $table
+     * @param Table $table
      *
      * @return void
      */
@@ -320,29 +121,6 @@ class FocalpointModellocation extends JModelAdmin
         } else {
             $table->fulldescription = '';
         }
-    }
-
-    /**
-     * Get defined custom fields for the selected location type
-     *
-     * @param int $type
-     *
-     * @return array
-     */
-    public function getCustomFields($type)
-    {
-        $db = $this->getDbo();
-
-        $query = $db->getQuery(true)
-            ->select('customfields')
-            ->from('#__focalpoint_locationtypes')
-            ->where('id = ' . (int)$type);
-
-        if ($customFields = $db->setQuery($query)->loadResult()) {
-            return json_decode($customFields, true);
-        }
-
-        return array();
     }
 
     /**
