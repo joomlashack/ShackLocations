11fab60702fbec2f0cadc1d54292e1bcf6f111bc
Bill Tomczak <bill@joomlashack.com>
2019-06-18 13:39:02 -0700

    Use cleaner js setup/rendering

--- src/site/views/map/tmpl/default_mapjs_uncommented.php
+++ src/site/views/map/tmpl/default_mapjs_uncommented.php
@@ -52,116 +52,160 @@ defined('_JEXEC') or die('Restricted access');
 JHtml::_('script', '//maps.googleapis.com/maps/api/js?key=' . $this->item->params->get('apikey'));
 JHtml::_('script', 'components/com_focalpoint/assets/js/infobox.js');
 
-$params             = JComponentHelper::getParams('com_focalpoint');
-$showlisttab        = $this->item->params->get('locationlist');
-$showmapsearch      = $this->item->params->get('mapsearchenabled');
-$mapsearchzoom      = $this->item->params->get('mapsearchzoom');
-$mapsearchrange     = $this->item->params->get('resultradius');
-$mapsearchprompt    = $this->item->params->get('mapsearchprompt');
-$searchassist       = ", ".$this->item->params->get('searchassist');
-$fitbounds          = $this->item->params->get('fitbounds');
-$markerclusters     = $this->item->params->get('markerclusters');
-$listtabfirst       = $this->item->params->get('showlistfirst');
+$params            = JComponentHelper::getParams('com_focalpoint');
+$showlisttab       = $this->item->params->get('locationlist');
+$showmapsearch     = $this->item->params->get('mapsearchenabled');
+$mapsearchzoom     = $this->item->params->get('mapsearchzoom');
+$mapsearchrange    = $this->item->params->get('resultradius');
+$mapsearchprompt   = $this->item->params->get('mapsearchprompt');
+$searchassist      = ", " . $this->item->params->get('searchassist');
+$zoom              = $this->item->params->get('zoom');
+$maxZoom           = $this->item->params->get('maxzoom', 'null');
+$zoomControl       = $this->item->params->get('zoomControl');
+$mapTypeControl    = $this->item->params->get('mapTypeControl');
+$scrollWheel       = $this->item->params->get('scrollwheel');
+$streetViewControl = $this->item->params->get('streetViewControl');
+$panControl        = $this->item->params->get('panControl');
+$draggable         = $this->item->params->get('draggable');
+$mapTypeId         = 'google.maps.MapTypeId.' . $this->item->params->get('mapTypeId');
+$mapStyle          = $this->item->params->get('mapstyle', "[]");
+$fitbounds         = (int)(bool)$this->item->params->get('fitbounds');
+$markerclusters    = (int)(bool)$this->item->params->get('markerclusters');
+$listtabfirst      = (int)(bool)$this->item->params->get('showlistfirst');
+$text              = (object)array(
+    'within'    => JText::_('COM_FOCALPOINT_WITHIN'),
+    'distance'  => JText::_('COM_FOCALPOINT_DISTANCE'),
+    'locations' => JText::_('COM_FOCALPOINT_LOCATIONS'),
+    'location'  => JText::_('COM_FOCALPOINT_LOCATION'),
+    'showing'   => JText::_('COM_FOCALPOINT_SHOWING'),
+    'notypes'   => JText::_('COM_FOCALPOINT_NO_LOCATION_TYPES_SELECTED')
+);
+
 if ($markerclusters) {
     JHtml::_('script', 'plugins/focalpoint/markerclusters/assets/markerclusterer.js');
 }
-$script ='
-	var allowScrollTo = false;
-	var searchTxt = "";
-	var showlisttab = '.$showlisttab.';
-	var showmapsearch = '.$showmapsearch.';
-	var mapsearchzoom = '.$mapsearchzoom.';
-	var mapsearchrange = '.$mapsearchrange.';
-	var mapsearchprompt = "'.$mapsearchprompt.'";
-	var searchassist = "'.$searchassist.'";
-	var fitbounds = '.($fitbounds?"1":"0").';
-	var markerclusters = '.($markerclusters?"1":"0").';
-	var listtabfirst = '.($listtabfirst?"1":"0").';
-	var map;
-	var mapCenter = new google.maps.LatLng('.$this->item->latitude.','.$this->item->longitude.');
-	var markerCluster;
-    var clusterMarkers = [];
-    if(typeof clusterStyles === \'undefined\') {
-        var clusterStyles = [];
+
+$script = <<<JSCRIPT
+var map             = null,
+    markerCluster   = null,
+    clusterMarkers  = [];
+    allowScrollTo   = false,
+    searchTxt       = '',
+    showlisttab     = {$showlisttab},
+    showmapsearch   = {$showmapsearch},
+    mapsearchzoom   = {$mapsearchzoom},
+    mapsearchrange  = {$mapsearchrange},
+    mapsearchprompt = '{$mapsearchprompt}',
+    searchassist    = '{$searchassist}',
+    fitbounds       = {$fitbounds},
+    markerclusters  = {$markerclusters},
+    listtabfirst    = {$listtabfirst},
+    mapCenter       = new google.maps.LatLng({$this->item->latitude}, {$this->item->longitude});
+
+if (typeof clusterStyles === 'undefined') {
+    var clusterStyles = [];
+}
+
+var marker = [];
+
+function updateActiveCount(marker) {
+    var locationTxt = '',
+        status      = '',
+        activeCount = 0;
+
+    jQuery.each(marker, function(i, m) {
+        if (typeof m !== 'undefined') {
+            if (marker[i].status > 0) {
+                activeCount += 1;
+                status = status + ' ' + marker[i].status;
+            }
+        }
+    });
+    if (searchTxt !== '') {
+        locationTxt = ' ({$text->within} ' + mapsearchrange + '{$text->distance} ' + searchTxt + ')';
     }
-    var marker= new Array();
-	function updateActiveCount(marker){
-		var locationTxt ="";
-		var status=""
-		var activeCount = 0;
-		jQuery.each(marker, function(i,m){
-			if(typeof m !== \'undefined\') {
-				if (marker[i].status > 0 ) {
-					activeCount += 1;
-					status = status+" "+marker[i].status;
-				}
-			}
-		});
-		if (searchTxt !="" ) {
-			locationTxt = " ('.JText::_("COM_FOCALPOINT_WITHIN").' "+mapsearchrange+"'.JText::_("COM_FOCALPOINT_DISTANCE").' "+searchTxt+")";
-		}
-		var locationPlural = "'.JText::_("COM_FOCALPOINT_LOCATIONS").'";
-		if (activeCount == 1) { locationPlural = "'.JText::_("COM_FOCALPOINT_LOCATION").'"; }
-		jQuery("#activecount").html("'.JText::_("COM_FOCALPOINT_SHOWING").' " + activeCount +" "+locationPlural+locationTxt+".");
-		if (activeCount == 0){
-			if (jQuery(".nolocations").length == 0){
-				jQuery("#fp_locationlist .fp_ll_holder").append("<div class=\"nolocations\">'.JText::_("COM_FOCALPOINT_NO_LOCATION_TYPES_SELECTED").'</div>");
-			}
-		} else {
-			jQuery(".nolocations").remove();
-		}
-	}
-    function initialize() {
-        var mapProp = {
-            center:new google.maps.LatLng('.$this->item->latitude.','.$this->item->longitude.'),
-            zoom:'.$this->item->params->get('zoom').',
-            maxZoom:'.$this->item->params->get('maxzoom','null').',
-            mapTypeControl: '.$this->item->params->get('mapTypeControl').',
-            zoomControl: '.$this->item->params->get('zoomControl').',
-            scrollwheel: '.$this->item->params->get('scrollwheel').',
-            streetViewControl: '.$this->item->params->get('streetViewControl').',
-            panControl: '.$this->item->params->get('panControl').',
-            draggable: '.$this->item->params->get('draggable').',
-            mapTypeId:google.maps.MapTypeId.'.$this->item->params->get('mapTypeId').',
-            styles: '.$this->item->params->get('mapstyle',"[]").'
-        };
-        map = new google.maps.Map(document.getElementById("fp_googleMap"),mapProp);
-        var markerSets = new Array();
-        var markerInfoBox = new Array();
-        var mappedMarkers = new Array();
-        var mapinfobox = false;
-        var columns = 4;
-        var columnCount = 0;
-';
+    var locationPlural = '{$text->locations}';
+    if (activeCount == 1) {
+        locationPlural = '{$text->location}';
+    }
+
+    jQuery('#activecount').html('{$text->showing}' + activeCount + ' ' + locationPlural + locationTxt + '.');
+
+    if (activeCount == 0) {
+        if (jQuery('.nolocations').length == 0) {
+            jQuery('#fp_locationlist .fp_ll_holder').append('<div class="nolocations">{$text->notypes}</div>');
+        }
+
+    } else {
+        jQuery('.nolocations').remove();
+    }
+}
+
+function initialize() {
+    var mapProperties = {
+        center           : new google.maps.LatLng({$this->item->latitude}, {$this->item->longitude}),
+        zoom             : {$zoom},
+        maxZoom          : {$maxZoom},
+        mapTypeControl   : {$mapTypeControl},
+        zoomControl      : {$zoomControl},
+        scrollwheel      : {$scrollWheel},
+        streetViewControl: {$streetViewControl},
+        panControl       : {$panControl},
+        draggable        : {$draggable},
+        mapTypeId        : {$mapTypeId},
+        styles           : {$mapStyle}
+    };
+    
+    map = new google.maps.Map(document.getElementById('fp_googleMap'), mapProperties);
+    var markerSets    = [],
+        markerInfoBox = [],
+        mappedMarkers = [],
+        mapinfobox    = false,
+        columns       = 4,
+        columnCount   = 0;
+JSCRIPT;
+
 
 // Cycle through each location creating a marker and infobox.
 foreach ($this->item->markerdata as $marker) {
     //Assemble the infobox.
     $marker->infodescription = "";
-    if ($marker->params->get('infoshowaddress') && $marker->address !="") $marker->infodescription .= "<p>".JText::_($marker->address)."</p>";
-    if ($marker->params->get('infoshowphone') && $marker->phone !="") $marker->infodescription .= "<p>".JText::_($marker->phone)."</p>";
-    if ($marker->params->get('infoshowintro') && $marker->description !="") $marker->infodescription .= "<p>".JText::_($marker->description)."</p>";
+    if ($marker->params->get('infoshowaddress') && $marker->address != "") {
+        $marker->infodescription .= "<p>" . JText::_($marker->address) . "</p>";
+    }
+    if ($marker->params->get('infoshowphone') && $marker->phone != "") {
+        $marker->infodescription .= "<p>" . JText::_($marker->phone) . "</p>";
+    }
+    if ($marker->params->get('infoshowintro') && $marker->description != "") {
+        $marker->infodescription .= "<p>" . JText::_($marker->description) . "</p>";
+    }
 
     // Example. If a custom fields was defined called "yourcustomfield" the following line would render
     // that field in the infobox and location list
-    if (!empty($marker->customfields->yourcustomfield->data)) $marker->infodescription .= $this->renderField($marker->customfields->yourcustomfield, true, true);
+    if (!empty($marker->customfields->yourcustomfield->data)) {
+        $marker->infodescription .= $this->renderField($marker->customfields->yourcustomfield, true, true);
+    }
 
-    $boxtext ='<h4>'.addslashes($marker->title).'</h4><div class=\"infoboxcontent\">'.addslashes(str_replace("src=\"images","src=\"".JUri::base(true)."/images",(str_replace(array("\n", "\t", "\r"), '', $marker->infodescription))));
-    if (isset($marker->link)) $boxtext.='<p class=\"infoboxlink\"><a title=\"'.addslashes($marker->title).'\" href=\"'.$marker->link.'\">'.JText::_('COM_FOCALPOINT_FIND_OUT_MORE').'</a></p>';
+    $boxtext = '<h4>' . addslashes($marker->title) . '</h4><div class=\"infoboxcontent\">' . addslashes(str_replace("src=\"images",
+            "src=\"" . JUri::base(true) . "/images",
+            (str_replace(array("\n", "\t", "\r"), '', $marker->infodescription))));
+    if (isset($marker->link)) {
+        $boxtext .= '<p class=\"infoboxlink\"><a title=\"' . addslashes($marker->title) . '\" href=\"' . $marker->link . '\">' . JText::_('COM_FOCALPOINT_FIND_OUT_MORE') . '</a></p>';
+    }
 
-    $boxtext.='<div class=\"infopointer\"></div></div>';
-    $script .= '
-    	if (jQuery.inArray('.$marker->id.' ,mappedMarkers) == -1) {
-			var myCenter'.$marker->id.'=new google.maps.LatLng('.$marker->latitude.','.$marker->longitude.');
-			marker['.$marker->id.']=new google.maps.Marker({
-				position:myCenter'.$marker->id.',
-				icon: "'.$marker->marker.'"
+    $boxtext .= '<div class=\"infopointer\"></div></div>';
+    $script  .= '
+    	if (jQuery.inArray(' . $marker->id . ' ,mappedMarkers) == -1) {
+			var myCenter' . $marker->id . '=new google.maps.LatLng(' . $marker->latitude . ',' . $marker->longitude . ');
+			marker[' . $marker->id . ']=new google.maps.Marker({
+				position:myCenter' . $marker->id . ',
+				icon: "' . $marker->marker . '"
 			});
-			var boxText'.$marker->id.' = "'.$boxtext.'";
-			markerInfoBox['.$marker->id.'] = new InfoBox({
-				content: boxText'.$marker->id.',
+			var boxText' . $marker->id . ' = "' . $boxtext . '";
+			markerInfoBox[' . $marker->id . '] = new InfoBox({
+				content: boxText' . $marker->id . ',
 				alignBottom: true,
-				position: new google.maps.LatLng('.$marker->latitude.','.$marker->longitude.'),
+				position: new google.maps.LatLng(' . $marker->latitude . ',' . $marker->longitude . '),
 				pixelOffset: new google.maps.Size(-160, -55),
 				maxWidth: 320,
 				zIndex: null,
@@ -173,37 +217,37 @@ foreach ($this->item->markerdata as $marker) {
                 contextMenu:true
             });
             if (markerclusters) {
-			    clusterMarkers.push(marker['.$marker->id.']);
+			    clusterMarkers.push(marker[' . $marker->id . ']);
 			} else {
-			    marker['.$marker->id.'].setMap(map);
+			    marker[' . $marker->id . '].setMap(map);
 			}
-			google.maps.event.addListener(marker['.$marker->id.'], \'click\', function() {
-				if (mapinfobox == markerInfoBox['.$marker->id.'] && mapinfobox.getVisible()) {
+			google.maps.event.addListener(marker[' . $marker->id . '], \'click\', function() {
+				if (mapinfobox == markerInfoBox[' . $marker->id . '] && mapinfobox.getVisible()) {
 					mapinfobox.close();
 				} else {
 					if (mapinfobox) {
 						mapinfobox.close()
 					}
-					mapinfobox = markerInfoBox['.$marker->id.'];
-					mapinfobox.open(map,marker['.$marker->id.']);
+					mapinfobox = markerInfoBox[' . $marker->id . '];
+					mapinfobox.open(map,marker[' . $marker->id . ']);
 				}
 			});
 			if (showlisttab) {
-				jQuery("#fp_locationlist .fp_ll_holder").append("<div class=\"fp_list_marker'.$marker->id.' fp_listitem\">"+boxText'.$marker->id.'+"</div>");
+				jQuery("#fp_locationlist .fp_ll_holder").append("<div class=\"fp_list_marker' . $marker->id . ' fp_listitem\">"+boxText' . $marker->id . '+"</div>");
 			}
-			marker['.$marker->id.'].status = 0;
-			marker['.$marker->id.'].lat='.$marker->latitude.';
-			marker['.$marker->id.'].lng= '.$marker->longitude.';
-			jQuery(".fp_list_marker'.$marker->id.'").status = 0;
+			marker[' . $marker->id . '].status = 0;
+			marker[' . $marker->id . '].lat=' . $marker->latitude . ';
+			marker[' . $marker->id . '].lng= ' . $marker->longitude . ';
+			jQuery(".fp_list_marker' . $marker->id . '").status = 0;
         }
-		marker['.$marker->id.'].status += 1;
-		jQuery(".fp_list_marker'.$marker->id.'").status +=1;
+		marker[' . $marker->id . '].status += 1;
+		jQuery(".fp_list_marker' . $marker->id . '").status +=1;
 
-        if(typeof markerSets['.$marker->locationtype_id.'] === \'undefined\') {
-            markerSets['.$marker->locationtype_id.'] = new Array();
+        if(typeof markerSets[' . $marker->locationtype_id . '] === \'undefined\') {
+            markerSets[' . $marker->locationtype_id . '] = new Array();
         }
-        mappedMarkers.push('.$marker->id.')
-        markerSets['.$marker->locationtype_id.'].push('.$marker->id.');
+        mappedMarkers.push(' . $marker->id . ')
+        markerSets[' . $marker->locationtype_id . '].push(' . $marker->id . ');
 
     ';
 }
@@ -283,8 +327,8 @@ $script .= '
                 if (newbounds) {
                     map.fitBounds(bounds);
                 } else {
-                    map.panTo(new google.maps.LatLng('.$this->item->latitude.','.$this->item->longitude.'));
-                    map.setZoom('.$this->item->params->get('zoom').');
+                    map.panTo(new google.maps.LatLng(' . $this->item->latitude . ',' . $this->item->longitude . '));
+                    map.setZoom(' . $this->item->params->get('zoom') . ');
                 }
             }
             if (markerclusters) {
@@ -314,12 +358,12 @@ $script .= '
         jQuery("ul.nav-tabs > li >a").click(function() {
             setTimeout(function(){
                 google.maps.event.trigger(map, \'resize\');
-                map.panTo(new google.maps.LatLng('.$this->item->latitude.','.$this->item->longitude.'));
-                map.setZoom('.$this->item->params->get('zoom').');
+                map.panTo(new google.maps.LatLng(' . $this->item->latitude . ',' . $this->item->longitude . '));
+                map.setZoom(' . $this->item->params->get('zoom') . ');
             },500); 
         });
         jQuery(window).resize(function() {
-            map.panTo(new google.maps.LatLng('.$this->item->latitude.','.$this->item->longitude.')); 
+            map.panTo(new google.maps.LatLng(' . $this->item->latitude . ',' . $this->item->longitude . ')); 
         });
 		jQuery("#fp_reset").click(function() {
 			allowScrollTo = false;
@@ -337,9 +381,9 @@ $script .= '
 				}
 			});
 			jQuery("#fp_toggle").each(function(){
-				if ("on" == "'.$this->item->params->get('showmarkers').'") {
+				if ("on" == "' . $this->item->params->get('showmarkers') . '") {
 					jQuery(this).data("togglestate","off");
-					jQuery(this).html("'. JText::_('COM_FOCALPOINT_BUTTTON_HIDE_ALL').'");
+					jQuery(this).html("' . JText::_('COM_FOCALPOINT_BUTTTON_HIDE_ALL') . '");
 					jQuery(".markertoggles").each(function(e){
 						if (jQuery(this).hasClass("active")) {
 							jQuery(this).trigger("click");
@@ -348,7 +392,7 @@ $script .= '
 					});
 				} else {
 					jQuery(this).data("togglestate","on");
-					jQuery(this).html("'. JText::_('COM_FOCALPOINT_BUTTTON_SHOW_ALL').'");
+					jQuery(this).html("' . JText::_('COM_FOCALPOINT_BUTTTON_SHOW_ALL') . '");
 					jQuery(".markertoggles").each(function(e){
 						if (jQuery(this).hasClass("active")) {
 							jQuery(this).trigger("click");
@@ -357,14 +401,14 @@ $script .= '
 				}
 			});
 			allowScrollTo = true;
-			map.panTo(new google.maps.LatLng('.$this->item->latitude.','.$this->item->longitude.'));
-			map.setZoom('.$this->item->params->get('zoom').');
+			map.panTo(new google.maps.LatLng(' . $this->item->latitude . ',' . $this->item->longitude . '));
+			map.setZoom(' . $this->item->params->get('zoom') . ');
 		});
 		jQuery("#fp_toggle").click(function() {
 			allowScrollTo = false;
 			if (jQuery(this).data("togglestate") == "on") {
 				jQuery(this).data("togglestate","off");
-				jQuery(this).html("'. JText::_('COM_FOCALPOINT_BUTTTON_HIDE_ALL').'");
+				jQuery(this).html("' . JText::_('COM_FOCALPOINT_BUTTTON_HIDE_ALL') . '");
 				jQuery(".markertoggles").each(function(e){
 					if (!jQuery(this).hasClass("active")) {
 						jQuery(this).trigger("click");
@@ -372,7 +416,7 @@ $script .= '
 				});
 			} else {
 				jQuery(this).data("togglestate","on");
-				jQuery(this).html("'. JText::_('COM_FOCALPOINT_BUTTTON_SHOW_ALL').'");
+				jQuery(this).html("' . JText::_('COM_FOCALPOINT_BUTTTON_SHOW_ALL') . '");
 				jQuery(".markertoggles").each(function(e){
 					if (jQuery(this).hasClass("active")) {
 						jQuery(this).trigger("click");
@@ -414,7 +458,7 @@ $script .= '
 						});
 						jQuery("#fp_toggle").each(function(){
 							jQuery(this).data("togglestate","off");
-							jQuery(this).html("'. JText::_('COM_FOCALPOINT_BUTTTON_HIDE_ALL').'");
+							jQuery(this).html("' . JText::_('COM_FOCALPOINT_BUTTTON_HIDE_ALL') . '");
 							jQuery(".markertoggles").each(function(e){
 								if (jQuery(this).hasClass("active")) {
 									jQuery(this).trigger("click");
@@ -483,7 +527,7 @@ $script .= '
             },100);
         }
 
-        if ("off" == "'.$this->item->params->get('showmarkers').'") {
+        if ("off" == "' . $this->item->params->get('showmarkers') . '") {
             setTimeout(function(){
                 jQuery("#fp_toggle").trigger("click");
             },100);
