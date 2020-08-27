033d23abaa018e6b24bc79da6059a34f2ac323cb
Bill Tomczak <bill@joomlashack.com>
2020-08-27 15:52:46 -0700

    Simplify legend buttons

--- src/site/views/map/tmpl/default_legend_buttons.php
+++ src/site/views/map/tmpl/default_legend_buttons.php
@@ -24,25 +24,36 @@
 
 defined('_JEXEC') or die('Restricted access');
 
-$showmapsearch = $this->item->params->get('mapsearchenabled');
-$searchprompt = $this->item->params->get('mapsearchprompt');
-$sidebar = ($this->item->params->get('legendposition') == "left" || $this->item->params->get('legendposition') == "right");
-?>
+$showMapSearch = $this->item->params->get('mapsearchenabled');
+$searchPrompt  = $this->item->params->get('mapsearchprompt');
+$sidebar       = $this->item->params->get('legendposition') == 'left'
+    || $this->item->params->get('legendposition') == 'right';
 
+$js = <<<JSCRIPT
+
+JSCRIPT;
+
+?>
 <div class="row-fluid ">
-	<p><small id="activecount"></small></p>
-	<div id="fp_map_actions">
-		<form onsubmit="return false;">
-            <?php if ($showmapsearch) { ?>
-            <div class="fp_mapsearch input-append">
-                <label for="fp_searchAddress"><?php echo $searchprompt; ?></label>
-                <input class="" id="fp_searchAddress" type="text" value="<?php echo $searchprompt; ?>" onblur="if (this.value=='') {this.value='<?php echo $searchprompt; ?>';jQuery('#fp_searchAddressBtn').attr('disabled', true);}" onfocus="if (this.value=='<?php echo $searchprompt; ?>') this.value='';jQuery('#fp_searchAddressBtn').attr('disabled', false);"><button class="btn " id="fp_searchAddressBtn" type="button" disabled >Go!</button>
-            </div>
-            <?php } ?>
+    <p><small id="activecount"></small></p>
+    <div id="fp_map_actions">
+        <form onsubmit="return false;">
+            <?php if ($showMapSearch) : ?>
+                <div class="fp_mapsearch input-append">
+                    <label for="fp_searchAddress"><?php echo $searchPrompt; ?></label>
+                    <input class=""
+                           id="fp_searchAddress"
+                           type="text"
+                           placeholder="<?php echo $searchPrompt; ?>">
+                    <button class="btn " id="fp_searchAddressBtn" type="button">Go!</button>
+                </div>
+            <?php endif; ?>
             <div id="fp_map_buttons" class="input-append">
-                <button class="btn btn-mini" id="fp_reset" onclick="return false;"><?php echo JText::_('COM_FOCALPOINT_BUTTON_RESET_MAP'); ?></button>
+                <button class="btn btn-mini"
+                        id="fp_reset"
+                        onclick="return false;"><?php echo JText::_('COM_FOCALPOINT_BUTTON_RESET_MAP'); ?></button>
                 <button class="btn btn-mini" id="fp_toggle" data-togglestate="on" onclick="return false;"></button>
             </div>
         </form>
-	</div>
+    </div>
 </div>
