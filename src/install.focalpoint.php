0e378d15058a7b576c83e582c70c1ef1f19344b4
Bill Tomczak <bill@joomlashack.com>
2021-02-11 13:28:31 -0800

    Add application property to installer

--- src/install.focalpoint.php
+++ src/install.focalpoint.php
@@ -23,6 +23,7 @@
  */
 
 use Alledia\Installer\AbstractScript;
+use Joomla\CMS\Application\CMSApplication;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Filesystem\File;
 use Joomla\CMS\Filesystem\Folder;
@@ -42,12 +43,25 @@ if (file_exists(__DIR__ . '/admin/library/Installer/AbstractScript.php')) {
 class com_focalpointInstallerScript extends AbstractScript
 {
     /**
+     * @var CMSApplication
+     */
+    protected $app = null;
+
+    /**
+     * @inheritDoc
+     */
+    public function __construct($parent)
+    {
+        parent::__construct($parent);
+
+        $this->app = Factory::getApplication();
+    }
+
+    /**
      * @inheritDoc
      */
     public function update($parent)
     {
-        $app = Factory::getApplication();
-
         try {
             if (parent::update($parent)) {
                 if (version_compare($this->previousManifest->version, '1.2', 'lt')) {
@@ -58,10 +72,10 @@ class com_focalpointInstallerScript extends AbstractScript
             }
 
         } catch (Exception $e) {
-            $app->enqueueMessage(sprintf('%s:%s<br>%s', $e->getFile(), $e->getLine(), $e->getMessage()));
+            $this->app->enqueueMessage(sprintf('%s:%s<br>%s', $e->getFile(), $e->getLine(), $e->getMessage()));
 
         } catch (Throwable $e) {
-            $app->enqueueMessage(sprintf('%s:%s<br>%s', $e->getFile(), $e->getLine(), $e->getMessage()));
+            $this->app->enqueueMessage(sprintf('%s:%s<br>%s', $e->getFile(), $e->getLine(), $e->getMessage()));
         }
 
         return false;
@@ -72,8 +86,6 @@ class com_focalpointInstallerScript extends AbstractScript
      */
     public function postflight($type, $parent)
     {
-        $app = Factory::getApplication();
-
         try {
             switch ($type) {
                 case 'install':
@@ -93,10 +105,10 @@ class com_focalpointInstallerScript extends AbstractScript
             parent::postFlight($type, $parent);
 
         } catch (Exception $e) {
-            $app->enqueueMessage(sprintf('%s:%s<br>%s', $e->getFile(), $e->getLine(), $e->getMessage()));
+            $this->app->enqueueMessage(sprintf('%s:%s<br>%s', $e->getFile(), $e->getLine(), $e->getMessage()));
 
         } catch (Throwable $e) {
-            $app->enqueueMessage(sprintf('%s:%s<br>%s', $e->getFile(), $e->getLine(), $e->getMessage()));
+            $this->app->enqueueMessage(sprintf('%s:%s<br>%s', $e->getFile(), $e->getLine(), $e->getMessage()));
         }
     }
 
