8762b2cd20a9daeca91221e7a3e1c0a6a8de600a
Bill Tomczak <bill@joomlashack.com>
2022-01-14 09:42:00 -0800

    Remove unused table field

--- src/admin/sql/updates/mysql/2.0.0.sql
+++ src/admin/sql/updates/mysql/2.0.0.sql
@@ -1,12 +1,12 @@
 -- MySQL Workbench Synchronization
--- Generated: 2022-01-06 17:04
+-- Generated: 2022-01-14 09:32
 -- Model: Shack Locations
 -- Version: 2.0.0
 -- Project: Shack Locations
 -- Author: Bill Tomczak
 
-SET @OLD_UNIQUE_CHECKS = @@UNIQUE_CHECKS, UNIQUE_CHECKS = 0;
-SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0;
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 
 ALTER TABLE `#__focalpoint_legends` CHANGE `created_by` `created_by` INT(11) NULL DEFAULT NULL AFTER `checked_out`;
 ALTER TABLE `#__focalpoint_legends` CHANGE `ordering` `ordering` INT(11) NOT NULL DEFAULT 0;
@@ -18,6 +18,7 @@ ALTER TABLE `#__focalpoint_locations` CHANGE `created_by` `created_by` INT(11) N
 ALTER TABLE `#__focalpoint_locations` CHANGE `checked_out` `checked_out` INT(11) NULL DEFAULT NULL AFTER `created_by`;
 ALTER TABLE `#__focalpoint_locations` CHANGE `checked_out_time` `checked_out_time` DATETIME NULL DEFAULT NULL AFTER `checked_out`;
 
+ALTER TABLE `#__focalpoint_locationtypes` DROP COLUMN `description`;
 ALTER TABLE `#__focalpoint_locationtypes` CHANGE `customfields` `customfields` TEXT NOT NULL AFTER `legend`;
 ALTER TABLE `#__focalpoint_locationtypes` CHANGE `created_by` `created_by` INT(11) NULL DEFAULT NULL AFTER `customfields`;
 ALTER TABLE `#__focalpoint_locationtypes` CHANGE `ordering` `ordering` INT(11) NOT NULL DEFAULT 0;
@@ -31,5 +32,5 @@ ALTER TABLE `#__focalpoint_maps` CHANGE `ordering` `ordering` INT(11) NOT NULL D
 ALTER TABLE `#__focalpoint_maps` CHANGE `centerpoint` `centerpoint` VARCHAR(300) NULL DEFAULT NULL;
 
 
-SET FOREIGN_KEY_CHECKS = @OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS = @OLD_UNIQUE_CHECKS;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
