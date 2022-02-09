25d26434c80fa0496bc66072d56376bc4c213e90
Bill Tomczak <bill@joomlashack.com>
2022-02-09 14:56:46 -0800

    Create base for Shack Locations plugins

---
+++ src/admin/library/joomla/plugin.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @package   ShackLocations-Pro
+ * @contact   www.joomlashack.com, help@joomlashack.com
+ * @copyright 2022 Joomlashack.com. All rights reserved
+ * @license   https://www.gnu.org/licenses/gpl.html GNU/GPL
+ *
+ * This file is part of ShackLocations-Pro.
+ *
+ * ShackLocations-Pro is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * ShackLocations-Pro is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ShackLocations-Pro.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+use Joomla\CMS\Plugin\CMSPlugin;
+
+defined('_JEXEC') or die();
+
+abstract class FocalpointPlugin extends CMSPlugin
+{
+    /**
+     * @inheritdoc
+     */
+    protected $autoloadLanguage = true;
+
+    /**
+     * @param int $mapId
+     *
+     * @return bool
+     */
+    protected function isMapExcluded(int $mapId): bool
+    {
+        $excludedMaps = array_map('intval', (array)$this->params->get('excludemaps'));
+
+        return in_array($mapId, $excludedMaps);
+    }
+}
